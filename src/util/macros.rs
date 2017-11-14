76e07772a93d92c7db0f98a01cb875ca3fa0ce19
qupeng <onlyqupeng@gmail.com>
2017-11-14 18:52:44 +0800

    make Statistics sync.

--- src/util/macros.rs
+++ src/util/macros.rs
@@ -187,3 +187,19 @@ macro_rules! try_opt {
         }
     });
 }
+
+#[macro_export]
+macro_rules! atomic_load {
+    ($atomic: expr) => {{
+        use ::std::sync::atomic::Ordering as AtomicOrdering;
+        ($atomic).load(AtomicOrdering::Acquire)
+    }}
+}
+
+#[macro_export]
+macro_rules! atomic_add {
+    ($atomic: expr, $rhs: expr) => {{
+        use ::std::sync::atomic::Ordering as AtomicOrdering;
+        ($atomic).fetch_add($rhs, AtomicOrdering::Release)
+    }}
+}
