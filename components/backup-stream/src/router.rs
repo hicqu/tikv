87f91921bd3901658ce36de8694034d32f5e010c
Yu Juncen <yu745514916@live.com>
2022-08-14 00:09:10 +0800

    fix build
    Signed-off-by: Yu Juncen <yu745514916@live.com>

--- components/backup-stream/src/router.rs
+++ components/backup-stream/src/router.rs
@@ -1266,8 +1266,9 @@ impl DataFile {
 
         for mut event in events.events {
             let mut decoded = event.key.clone();
-            decode_bytes_in_place(&mut decoded, false)
-                .unwrap_or(|| panic!("meet key cannot be decoded {}", utils::redact(&key)));
+            decode_bytes_in_place(&mut decoded, false).unwrap_or_else(|| {
+                panic!("meet key cannot be decoded {}", utils::redact(&event.key))
+            });
             if let Ok(2590) = decode_int_handle(&decoded) {
                 let tbl_id = decode_table_id(&decoded).unwrap_or_default();
                 info!("2590 meet"; "table" => %tbl_id, "key" => %redact(&decoded));
